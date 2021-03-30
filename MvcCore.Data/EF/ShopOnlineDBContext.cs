22e058980057441c8e25b271d4bc4355394adb99
leminhtri <lmtri1908@gmail.com>
2021-03-30 17:20:45 +0700

    Add_DbSet_ShoponlineDbContext

--- MvcCore.Data/EF/ShopOnlineDBContext.cs
+++ MvcCore.Data/EF/ShopOnlineDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MvcCore.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,7 +12,28 @@ namespace MvcCore.Data.EF
         {
            
         }
-        
-        ///public DbSet<>
+
+        public DbSet<Actions> Actions { get; set; }
+        public DbSet<AppConfigration> Configrations { get; set; }
+        public DbSet<AppRoles> AppRoles { get; set; }
+        public DbSet<AppUsers> AppUsers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryTranslation> TranslationCategories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Funtions> Funtions { get; set; }
+        public DbSet<Languages> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ProductCategories> ProductCategories { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<SystemActivities> SystemActivities { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransactionCategory> TransactionCategories { get; set; }
+        public DbSet<UserRoles> UserRoles { get; set; }
     }
 }
